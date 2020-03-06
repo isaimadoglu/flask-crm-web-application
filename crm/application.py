252ca53685fee2cafece4187eee558e518a4d897
İsa İmadoğlu <isaimadoglu@gmail.com>
2020-03-06 21:34:09 +0300

    Class codes of "Lead" database table is completed.
    route function was coded for "index.html" page.
    db.create_all() and app.run(debug=True) functions are used so as to start app appropriately.

--- crm/application.py
+++ crm/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 import os # to give path of sqlite3 database
@@ -11,4 +11,24 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ORM_DIR
 db = SQLAlchemy(app)
 
+@app.route("/")
+def index():
+  return render_template("index.html")
+
 # classes for database will be created.
+class Lead(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(40))
+  company = db.Column(db.String(80))
+  phone = db.Column(db.String(20))
+  email = db.Column(db.String(40))
+
+class Touch(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  #lead = db.Column()
+  #date = db.Column()
+  description = db.Column(db.String(200))
+  
+if __name__ = "__main__":
+  db.create_all()
+  app.run(debug=True)
